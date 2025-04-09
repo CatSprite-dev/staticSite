1e32e0f9f183ac992a91493a8baf5b04eacfee38
ThePrimeagen <catspriteee@gmail.com>
2025-04-09 19:26:00 +0300

    Finished with nodes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise Exception("Undefined text type")
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.text, "alt": text_node.url})
+
+
+
 def main():
     textnode_obj = TextNode("мой текст", TextType.BOLD, "http//:my_text")
 
@@ -25,5 +45,8 @@ def main():
     print(f"3. {leafnode_obj.__repr__()}")
     print(f"4. {parentnode_obj.__repr__()}")
     print(f"5: {parentnode_obj.to_html()}")
+
+
+
 
 main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-
+from main import text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -24,6 +24,35 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.LINK, url="https://different.com")
         self.assertNotEqual(node, node2)
         
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        image_url = "https://example.com/image.jpg"
+        alt_text = "Example image"
+        node = TextNode(image_url, TextType.IMAGE, alt_text)
+        
+        html_node = text_node_to_html_node(node)
+    
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(len(html_node.props), 2)
+        self.assertEqual(html_node.props["src"], image_url)
+        self.assertEqual(html_node.props["alt"], alt_text)
+    
+    def test_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "https://example.com/image.jpg")
+
+        html_node = text_node_to_html_node(node)
+
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(len(html_node.props), 1)
+        self.assertEqual(html_node.props["href"], "https://example.com/image.jpg")
+
 
 if __name__ == "__main__":
     unittest.main()
