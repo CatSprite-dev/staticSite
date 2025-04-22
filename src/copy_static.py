4231b1802be1129132ec57a2be8a5648f8d12a9e
ThePrimeagen <catspriteee@gmail.com>
2025-04-22 23:26:52 +0300

    Added a functiom which copy static files into public dir

---
+++ src/copy_static.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+def delete_public(public_path):
+        if os.path.exists(public_path):
+            return shutil.rmtree(public_path)
+        return
+
+def generate_new_public(public_path):
+    if os.path.exists(public_path) == False:
+        return os.mkdir(public_path)
+    return
+
+def copy_static(path_src, path_des):
+    file_list = os.listdir(path_src)
+    for file in file_list:
+        if os.path.isfile(os.path.join(path_src, file)):
+            shutil.copy(os.path.join(path_src, file), path_des)
+        else:
+            os.mkdir(os.path.join(path_des, file))
+            new_src_path = os.path.join(path_src, file)
+            new_des_path = os.path.join(path_des, file)
+            copy_static(new_src_path,new_des_path)
+                
--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from copy_static import *
 
+static_path = "./static"
+public_path = "./public"
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    print("Deleting public directory...")
+    delete_public(public_path)
 
-
-
+    print("Copying static files to public directory...")
+    generate_new_public(public_path)
+    copy_static(static_path, public_path)
 
 main()
