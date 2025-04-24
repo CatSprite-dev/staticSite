8b8085faf92a62d40fc4c3afdbeead9c4f8ed0db
ThePrimeagen <catspriteee@gmail.com>
2025-04-25 00:20:37 +0300

    Host with github

--- src/generate_page.py
+++ src/generate_page.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
                     return line.strip("#").strip()
         raise Exception("No title")
     
-def generate_page(md_file_path, template_path, dest_path):
+def generate_page(md_file_path, template_path, dest_path, basepath):
     print(f"Generating page from {md_file_path} to {dest_path} using {template_path}")
     #file_list = os.listdir(from_path)
     #for file in file_list:
@@ -27,22 +27,25 @@ def generate_page(md_file_path, template_path, dest_path):
     title = extract_title(content)
     full_html_content = template.replace("{{ Title }}", title)
     full_html_content = full_html_content.replace("{{ Content }}", html_content)
+    full_html_content = full_html_content.replace('href="/', f'href="{basepath}')
+    full_html_content = full_html_content.replace('src="/', f'src="{basepath}')
+
 
     file_out = open(os.path.join(dest_path, "index.html"), "w")
     file_out.write(full_html_content)
     file_out.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     file_list = os.listdir(dir_path_content)
     for file in file_list:
         if os.path.isfile(os.path.join(dir_path_content, file)):
             file_path = os.path.join(dir_path_content, file)
             public_path = os.path.dirname(os.path.join(dest_dir_path, file))
-            generate_page(file_path, template_path, public_path)
+            generate_page(file_path, template_path, public_path, basepath)
         else:
             copy_static(dir_path_content, dest_dir_path)
             new_src_path = os.path.join(dir_path_content, file)
             new_dest_path = os.path.join(dest_dir_path, file)
-            generate_pages_recursive(new_src_path, template_path, new_dest_path)
+            generate_pages_recursive(new_src_path, template_path, new_dest_path, basepath)
 
         
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+import sys
 from generate_page import generate_page, generate_pages_recursive
 from copy_static import *
+
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
 content_path = "./content"
 template_path = "template.html"
 static_path = "./static"
-public_path = "./public"
+public_path = "./docs"
 
 def main():
     print("Deleting public directory...")
@@ -13,6 +16,6 @@ def main():
     print("Copying static files to public directory...")
     generate_new_public(public_path)
     copy_static(static_path, public_path)
-    generate_pages_recursive(content_path, template_path, public_path)
+    generate_pages_recursive(content_path, template_path, public_path, basepath)
 
 main()
